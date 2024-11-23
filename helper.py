84b370a40afaf17ece606ffb4c6f9613f1e350b4
NvkAnirudh <nuti.krish4@gmail.com>
2024-11-23 02:20:46 -0500

    Updated helper.py

--- helper.py
+++ helper.py
@@ -3,6 +3,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 import json
+import gradio as gr
 
 # these expect to find a .env file at the directory above the lesson. 
 # the format for that file is (without the comment)                                                                           
@@ -22,3 +23,68 @@ def get_together_api_key():
      load_env()
      together_api_key = os.getenv("TOGETHER_API_KEY")
      return together_api_key
+
+def start_game(main_loop, share=False):
+    demo = gr.ChatInterface(
+        main_loop,
+        chatbot=gr.Chatbot(height=250, placeholder="Type 'start game' to begin"),
+        textbox=gr.Textbox(placeholder='What do you do next?', container=False, scale=7),
+        title='AI RPG',
+        theme='soft',
+        examples=['Look Around', 'Continue the story'],
+        cache_examples=False
+    )
+
+    demo.launch(share=share, server_name='0.0.0.0')
+
+def run_action(message, history, game_state):
+    if message == 'start_game':
+        return game_state['start']
+
+    system_prompt = """You are an AI Game master. Your job is to write what \
+happens next in a player's adventure game.\
+Instructions: \
+You must on only write 1-3 sentences in response. \
+Always write in second person present tense. \
+Ex. (You look north and see...)"""
+
+    world_info = f"""
+    World: {game_state['world']}
+    Kingdom: {game_state['kingdom']}
+    Town: {game_state['town']}
+    Your Character: {game_state['character']}"""
+
+    messages = [
+        {'role':'system', 'content':system_prompt},
+        {'role':'user', 'content':world_info}
+    ]
+
+    for action in history:
+        messages.append({'role':'assistant', 'content':action[0]})
+        messages.append({'role':'user', 'content':action[1]})
+    
+    messages.append({'role':'user', 'content':message})
+    model_output = client.chat.completions.create(
+        model="meta-llama/Llama-3-70b-chat-hf",
+        messages=messages
+    )
+    
+    result = model_output.choices[0].message.content
+    return result
+
+def get_game_state():
+    world = load_world('./data/Westeros.json')
+    kingdom = world['kingdoms']['Emberhaven']
+    town = kingdom['towns']["Dragon's Watch"]
+    character = town['npcs']['Eira Shadowglow']
+    start = world['start']
+
+    game_state = {
+    "world": world['description'],
+    "kingdom": kingdom['description'],
+    "town": town['description'],
+    "character": character['description'],
+    "start": start,
+    }
+
+    return game_state
